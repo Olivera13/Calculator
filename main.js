77ec4c565c2ad4c6ea6dad6aef91d3ef1147cb47
Olivera13 <48322232+Olivera13@users.noreply.github.com>
2020-08-11 13:45:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+/*var btn=$('#btn');
+btn.on('click',function(){
+   alert($('#naslov').text());
+})*/
+
+var btn=$('#btn');
+btn.on('click',function(){
+  /* console.log($(".dd").html());
+    console.log($("#naslov").css("font-size"));
+    
+   $("#naslov").css("font-size","64px");
+    
+     $("#naslov").text("Moj naslov");*/
+    
+    $(".dd").html("<h1>New Times</h1>");
+})
